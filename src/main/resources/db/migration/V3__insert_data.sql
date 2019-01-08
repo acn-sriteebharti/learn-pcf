f4a1783906870d8273bddaabe98e800d58dfa8d1
sritee.bharti <sritee.bharti@accenture.com>
2019-01-08 09:18:40 -0500

    adding a squad list

---
+++ src/main/resources/db/migration/V3__insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO wt_teams(id,team_name,rank_no,position_no,team_desc,image) VALUES (14,'Tachikawa Squad','A',1,NULL,NULL);
+INSERT INTO wt_teams(id,team_name,rank_no,position_no,team_desc,image) VALUES (15,'Fuyushima Squad','A',2,NULL,NULL);
+INSERT INTO wt_teams(id,team_name,rank_no,position_no,team_desc,image) VALUES (16,'Kazama Squad','A',3,NULL,NULL);
+INSERT INTO wt_teams(id,team_name,rank_no,position_no,team_desc,image) VALUES (17,'Kusakabe Squad','A',4,NULL,NULL);
+INSERT INTO wt_teams(id,team_name,rank_no,position_no,team_desc,image) VALUES (18,'Arashiyama Squad','A',5,NULL,NULL);
+INSERT INTO wt_teams(id,team_name,rank_no,position_no,team_desc,image) VALUES (19,'Kako Squad','A',6,NULL,NULL);
+INSERT INTO wt_teams(id,team_name,rank_no,position_no,team_desc,image) VALUES (20,'Miwa Squad','A',7,NULL,NULL);
